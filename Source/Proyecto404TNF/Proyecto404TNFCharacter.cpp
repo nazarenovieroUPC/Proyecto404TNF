d00eb2e239d3a3be29be752ebdd74d2aae13d1e7
nazarenovieroUPC <nazarenoviero@upc.edu.ar>
2026-04-27 21:54:02 -0300

    Cambio de estructura de carpetas
    -Carpetas nuevas en Game.
    -Cambio de localizacion de actores dentro de Game/BluePrints/Actors
    -Cambio de localizacion de character dentro de Game/BluePrints/Characters
    -Creación GameMode nuevo para ser configurable.

--- Source/Proyecto404TNF/Proyecto404TNFCharacter.cpp
+++ Source/Proyecto404TNF/Proyecto404TNFCharacter.cpp
@@ -212,6 +212,7 @@ void AProyecto404TNFCharacter::AttackMelee(const FInputActionValue& Value)
 		
 		AnimationSwordAttack();
 		
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Emerald, "CombatComponent");
 		if (CombatComponent){ CombatComponent -> MeleeAtack(Damage); }
 	}
 }
