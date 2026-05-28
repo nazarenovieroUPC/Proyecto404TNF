95fd90ab9b1f70eedc00995fcd8942965a1c0441
nazarenovieroUPC <nazarenoviero@upc.edu.ar>
2026-05-28 17:35:04 -0300

    Enemies Patrol
    -BTTasks de patrullaje aleatorio
    -Configuracion de Perception
    -Seguimiento de Player

---
+++ Source/Proyecto404TNF/Public/Actors/Characters/Enemies/AI/EnemyAIController.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/AIModule/Classes/AIController.h"
+#include "EnemyAIController.generated.h"
+
+struct FAIStimulus;
+class UAISenseConfig_Sight;
+
+UCLASS()
+class PROYECTO404TNF_API AEnemyAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AEnemyAIController();
+	
+	UPROPERTY(Blueprintable, EditAnywhere, BlueprintReadWrite, Category = "Behavior Tree")
+	TObjectPtr<UBehaviorTree> BehaviorTree;
+	
+	UPROPERTY(Blueprintable, EditAnywhere, BlueprintReadWrite, Category = "Perception")
+	TObjectPtr<UAISenseConfig_Sight> SightConfig;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void OnPossess(APawn* InPawn) override;
+	
+	UFUNCTION()
+	void OnTargetDetected(AActor* Actor, FAIStimulus Stimulus);
+};
---
+++ Source/Proyecto404TNF/Public/Actors/Characters/Enemies/AI/Tasks/BTTask_Patrol.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_Patrol.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROYECTO404TNF_API UBTTask_Patrol : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+public:
+	UBTTask_Patrol();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
+	float PatrolRadius;
+	
+private:
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+public:
+};
